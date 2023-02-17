7beefa37d230aed22d961382ab45d3834765a61d
antunes2603 <antunes2603@gmail.com>
2023-02-17 14:49:49 +0000

    app.module.reverse

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,23 @@ import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { MainModule } from './main/main.module';
 import { LoginComponent } from './login/login.component';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        LoginComponent,
-    ],
-    providers: [],
-    bootstrap: [AppComponent],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        RouterModule.forRoot([
-            { path: "login", component: LoginComponent },
-            { path: "", redirectTo: "login", pathMatch: "full" },
-            { path: "**", redirectTo: "login", pathMatch: "full" },
-        ]),
-        HttpClientModule,
-        MainModule,
-    ]
+  declarations: [
+    AppComponent,
+    LoginComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: "login", component:LoginComponent},
+      {path: "", redirectTo: "login",pathMatch: "full"},
+      {path: "**", redirectTo: "login",pathMatch: "full"},
+    ]),
+    MainModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
